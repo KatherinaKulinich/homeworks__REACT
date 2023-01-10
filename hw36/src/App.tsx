b24a11ec2f914bdd24002cca1c23fd7be0cef2e9
KatherinaKulinich <kulinichcatherina@gmail.com>
2023-01-10 15:58:26 +0200

    hw36 done

---
+++ hw36/src/App.tsx
@@ -0,0 +1,13 @@
+import './App.css'
+import Container from './components/Container'
+
+function App() {
+
+  return (
+    <div className="app">
+      <Container/>
+    </div>
+  )
+}
+
+export default App
